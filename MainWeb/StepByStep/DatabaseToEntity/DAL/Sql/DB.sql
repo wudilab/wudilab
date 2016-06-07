c7547540de1ada577d62e81ffdd3a420e2ceb3f1
wudilab <wudilinux@gmail.com>
2016-06-07 10:49:16 +1000

    make empty entity framework to code successfully

---
+++ MainWeb/StepByStep/DatabaseToEntity/DAL/Sql/DB.sql
@@ -0,0 +1,49 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Course') AND type in (N'U'))
+    DROP TABLE dbo.Course
+GO
+CREATE TABLE dbo.Course(
+       CourseId int identity(1,1) PRIMARY KEY,
+       Title nvarchar(128) default '' not null,
+       Credits int default 1 not null
+)
+GO
+
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Student') AND type in (N'U'))
+    DROP TABLE dbo.Student
+GO
+CREATE TABLE dbo.Student(
+   StudentId int identity(1,1) PRIMARY KEY,
+   LastName nvarchar(64) not null,
+   FirstMidName nvarchar(64) not null,
+   EnrollmentDate DateTime not null
+)
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Enrollment') AND type in (N'U'))
+    DROP TABLE dbo.Enrollment
+GO
+CREATE TABLE dbo.Enrollment(
+        EnrollmentId int identity(1,1) PRIMARY KEY,
+        CourseId int not null,
+        StudentId int not null,
+        Grade int default 0 not null
+)
+GO
+
+  ALTER TABLE [dbo].[Enrollment]  WITH CHECK ADD  CONSTRAINT [FK_Enrollment_Student] FOREIGN KEY([StudentId])
+        REFERENCES [dbo].[Student] ([StudentId])
+  GO
+
+
+    ALTER TABLE [dbo].[Enrollment]  WITH CHECK ADD  CONSTRAINT [FK_Enrollment_Course] FOREIGN KEY([CourseId])
+        REFERENCES [dbo].[Course] ([CourseId])
+  GO
+
+
+
+
+
+
+
+
--- MainWeb/StepByStep/DatabaseToEntity/DatabaseToEntity/Program.cs
+++ MainWeb/StepByStep/DatabaseToEntity/DatabaseToEntity/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,16 @@ namespace DatabaseToEntity
     {
         static void Main(string[] args)
         {
+            SchoolContainer school = new SchoolContainer();
+
+            DbSet<Student> students = school.Students;
+
+            foreach(Student s in students)
+            {
+                Console.WriteLine(s.EnrollmentDate);
+                Console.WriteLine(s.LastName);
+                Console.WriteLine(s.FirstMidName);
+            }
         }
     }
 }
