0a04028b3c806219cdcdee443eaaa05c2b92bb9b
wudilab <wudilinux@gmail.com>
2016-06-07 16:54:02 +1000

    Add Login Authentication Test MVC Web Site

---
+++ MainWeb/StepByStep/LoginAuthentication/LoginAuthentication/LoginAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginAuthentication.Startup))]
+namespace LoginAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
