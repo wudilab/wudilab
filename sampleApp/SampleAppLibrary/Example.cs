6a5170e806248ae6d1f8404a77f611be1e7c0fa0
wudilab <wudilinux@gmail.com>
2016-06-12 23:07:45 +1000

    add ionic call dll sample

---
+++ sampleApp/SampleAppLibrary/Example.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAppLibrary
+{
+    public sealed class Example
+    {
+        public static string GetAnswer(string answer)
+        {
+            return answer;
+        }
+
+        public int SampleProperty { get; set; }
+    }
+}
