f75fe1a40350047af5dbbfd9d7b0fe93eb282bc1
wudilab <wudilinux@gmail.com>
2016-07-03 01:02:01 +1000

    add leetcode main

---
+++ MainWeb/LeetcodeMain/MissingNumber268.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeMain
+{
+    public class MissingNumber268
+    {
+        public int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i)
+                    return i;
+            }
+            return nums.Length;
+        }
+    }
+}
---
+++ MainWeb/LeetcodeMain/ProductofArrayExceptSelf238.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeMain
+{
+    public class ProductofArrayExceptSelf238
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] p = new int[nums.Length];
+
+            p[0] = 1;
+
+            for(int i=1;i<nums.Length;i++)
+            {
+                p[i] = p[i - 1] * nums[i - 1];
+            }
+
+            for(int i=nums.Length-1;i>=0;i--)
+            {
+
+            }
+            return p;
+        }
+    }
+}
