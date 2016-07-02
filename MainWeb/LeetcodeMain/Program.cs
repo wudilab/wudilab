abe1d35d4cab9d5b685b04796856bbb49044ac04
wudilab <wudilinux@gmail.com>
2016-07-02 18:12:31 +1000

    add Leetcode for C#

---
+++ MainWeb/LeetcodeMain/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumofTwoIntegers s = new SumofTwoIntegers();
+            Console.WriteLine(s.GetSum(0, 0));
+        }
+    }
+}
---
+++ MainWeb/LeetcodeMain/SumofTwoIntegers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeMain
+{
+    public class SumofTwoIntegers
+    {
+        public int GetSum(int a, int b)
+        {
+            int sum = a;
+            int carry = b;
+
+            while (carry != 0)
+            {
+                int tmps = sum;
+
+                sum = tmps ^ carry;
+                carry = (tmps & carry) << 1;
+            }
+
+            return sum;
+        }
+    }
+}
